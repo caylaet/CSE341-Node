7fb81f513e8b0c3029e40b1a307efc39d8918236
CaylaT <72427853+caylaet@users.noreply.github.com>
2022-01-28 00:09:28 -0500

    Week 4
    Added for heroku

--- Section13/app.js
+++ Section13/app.js
@@ -7,6 +7,22 @@ const mongoose = require('mongoose');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const PORT = process.env.PORT || 3000; // So we can run on heroku || (OR) localhost:5000
+
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+
+const corsOptions = {
+  origin: "https://cse341-caylatribett-node.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+  family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://nodeJSTalk:f7KvKR6YKygZhik@cluster0.copz7.mongodb.net/shop?retryWrites=true&w=majority";
+
 
 const app = express();
 
@@ -33,7 +49,7 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://nodeJSTalk:f7KvKR6YKygZhik@cluster0.copz7.mongodb.net/shop?retryWrites=true&w=majority')
+mongoose.connect(MONGODB_URL, options)
 .then( result => {
   User.findOne().then( user1 => {
     if(!user1){
@@ -47,7 +63,7 @@ mongoose.connect('mongodb+srv://nodeJSTalk:f7KvKR6YKygZhik@cluster0.copz7.mongod
     user.save();
     }
   });
-  app.listen(3000);
+  app.listen(PORT);
 
 })
 .catch( err => {
